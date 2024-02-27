ff3d555b6ae8ca581bbf1375e364fa9b94d7c248
K0bus <contact@k0bus.fr>
2024-02-27 22:22:16 +0100

    first commit

---
+++ src/main/java/fr/k0bus/restrictionmanager/RestrictionManage.java
@@ -0,0 +1,17 @@
+package fr.k0bus.restrictionmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RestrictionManage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
